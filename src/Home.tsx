9315679855c564e656ee95ef93c48245e691298b
Aliux7 <kelsonedbert@gmail.com>
2024-04-30 13:05:02 +0700

    feats: change frontend(1)

--- src/Home.tsx
+++ src/Home.tsx
@@ -5,6 +5,7 @@ import { useNavigate, useParams } from 'react-router-dom';
 import { user_backend } from './declarations/user_backend';
 import { Principal } from '@ic-reactor/react/dist/types';
 import { User } from './declarations/user_backend/user_backend.did';
+import Header from './components/ui/Header';
 
 type UserState = User[] | [];
 
@@ -77,19 +78,32 @@ function HomePage() {
     };
 
     return (
-        <div>
-            This is home page
-            <br></br>
-            {authenticated ? (
-                <div>Welcome, {currentUser.at(0)?.name.toString()}</div>
-            ) : (
-                <div>You are not logged in yet</div>
-            )}
-            {authenticated ? (
-                <button onClick={handleLogout}>Logout</button>
-            ) : (
-                <button onClick={handleLogin}>Login</button>
-            )}
+        <div className=''>
+            <Header/>
+            <main className='bg-black w-full h-[150vh] text-white pt-[100px] flex flex-col  items-center'>
+                
+                <a href="#" className="flex flex-col items-center border rounded-lg shadow md:flex-row md:max-w-xl border-gray-700 bg-black hover:bg-gray-900">
+                    <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="/assets/fundwise.png" alt=""/>
+                    <div className="flex flex-col justify-between p-4 leading-normal">
+                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-white">Binus</h5>
+                        <p className="mb-3 font-normal text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                        <p className="mb-3 font-normal text-gray-400">Ended 107% Funded</p>
+                    </div>
+                </a>
+
+                This is home page
+                <br></br>
+                {authenticated ? (
+                    <div>Welcome, {currentUser.at(0)?.name.toString()}</div>
+                ) : (
+                    <div>You are not logged in yet</div>
+                )}
+                {authenticated ? (
+                    <button onClick={handleLogout}>Logout</button>
+                ) : (
+                    <button onClick={handleLogin}>Login</button>
+                )}
+            </main>
         </div>
     );
 }
