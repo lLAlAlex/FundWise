a7cbac7911fe53d7c34d91389089aaaf9d7f7869
Nicholas Chandra <nicoch4n@gmail.com>
2024-05-06 20:40:21 +0700

    fix login

--- src/pages/auth/register/Register.tsx
+++ src/pages/auth/register/Register.tsx
@@ -10,13 +10,19 @@ function RegisterPage() {
   const [errorMsg, setErrorMsg] = useState('');
   const navigate = useNavigate();
 
+
   const [formData, setFormData] = useState({
     name: '',
     email: '',
     dob: '',
     timestamp: '',
     profile: '',
+    description: '',
+    location: '',
+    contact: '',
+    status: ''
   });
+
 
   const handleChange = (e: { target: { name: any; value: any } }) => {
     const { name, value } = e.target;
@@ -31,29 +37,26 @@ function RegisterPage() {
     const identity = await authClient.getIdentity();
     const principal = identity.getPrincipal();
 
+
     if (!formData.name || !formData.email) {
       setErrorMsg('All fields must be filled');
-    } else if (principal.toString() === '2vxsx-fae') {
+    }
+    else if (principal.toString() === '2vxsx-fae') {
       return;
-    } else {
-      formData.profile =
-        'https://res.cloudinary.com/dogiichep/image/upload/v1691980787/profile_xy1yuo.png';
-      await user_backend.register(
-        principal,
-        formData.name,
-        formData.email,
-        formData.profile,
-        '04-12-2003',
-      );
+    }
+    else {
+      formData.profile = "https://res.cloudinary.com/dogiichep/image/upload/v1691980787/profile_xy1yuo.png"
+      console.log(formData);
+      await user_backend.register(principal, formData.name, formData.email, formData.profile, formData.dob, formData.location, formData.contact);
       return navigate('/');
     }
   };
 
-  // useEffect(() => {
-  //   if (auth) {
-  //     navigate('/');
-  //   }
-  // }, [auth]);
+  useEffect(() => {
+    if (auth) {
+      navigate('/');
+    }
+  }, [auth]);
 
   return (
     <section className="bg-gray-50 dark:bg-gray-900">
@@ -109,30 +112,80 @@ function RegisterPage() {
                   value={formData.email}
                 />
               </div>
-              <div className="flex items-start">
-                <div className="flex items-center h-5">
-                  <input
-                    id="terms"
-                    aria-describedby="terms"
-                    type="checkbox"
-                    className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                  />
-                </div>
-                <div className="ml-3 text-sm">
-                  <label
-                    htmlFor="terms"
-                    className="font-light text-gray-500 dark:text-gray-300"
-                  >
-                    I accept the{' '}
-                    <a
-                      className="font-medium text-primary-600 hover:underline dark:text-primary-500"
-                      href="#"
-                    >
-                      Terms and Conditions
-                    </a>
-                  </label>
-                </div>
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Date of Birth
+                </label>
+                <input
+                  type="date"
+                  name="dob"
+                  id="dob"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  onChange={handleChange}
+                  value={formData.dob}
+                />
               </div>
+              <div>
+                <label
+                  htmlFor="text"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Location
+                </label>
+                <input
+                  type="text"
+                  name="location"
+                  id="location"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Indonesia"
+                  onChange={handleChange}
+                  value={formData.location}
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="text"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Contact
+                </label>
+                <input
+                  type="text"
+                  name="contact"
+                  id="contact"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="+62xxxxxxxxxxx"
+                  onChange={handleChange}
+                  value={formData.contact}
+                />
+              </div>
+              {/* <div className="flex items-start">
+                                <div className="flex items-center h-5">
+                                    <input
+                                        id="terms"
+                                        aria-describedby="terms"
+                                        type="checkbox"
+                                        className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                                    />
+                                </div>
+                                <div className="ml-3 text-sm">
+                                    <label
+                                        htmlFor="terms"
+                                        className="font-light text-gray-500 dark:text-gray-300"
+                                    >
+                                        I accept the{" "}
+                                        <a
+                                            className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                                            href="#"
+                                        >
+                                            Terms and Conditions
+                                        </a>
+                                    </label>
+                                </div>
+                            </div> */}
               <button
                 type="submit"
                 className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
@@ -144,7 +197,14 @@ function RegisterPage() {
           </div>
         </div>
       </div>
-    </section>
+      <button
+        type="submit"
+        className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+        onClick={handleSubmit}
+      >
+        Create an account
+      </button>
+    </section >
   );
 }
 
