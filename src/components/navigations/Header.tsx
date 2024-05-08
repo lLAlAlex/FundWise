f175935d1785e5df7877638ea0a8c475873155a6
Aliux7 <kelsonedbert@gmail.com>
2024-05-08 13:51:29 +0700

    feats: redesign home UI

--- src/components/navigations/Header.tsx
+++ src/components/navigations/Header.tsx
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion';
 import { useState, useEffect } from 'react';
 import { useAuth, useQueryCall, useUpdateCall } from '@ic-reactor/react';
 import { AuthClient } from '@dfinity/auth-client';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import {
   user_backend,
   canisterId,
@@ -15,6 +15,9 @@ import {
 } from '../../declarations/user_backend/user_backend.did';
 import useLogin from '@/hooks/auth/login/useLogin';
 import useAuthentication from '@/hooks/auth/get/useAuthentication';
+import { Container } from '../ui/Container';
+import { Hamburger } from '../ui/Hamburger';
+import classNames from 'classnames';
 
 
 const Header = () => {
@@ -33,6 +36,7 @@ const Header = () => {
   const { loginStatus, login } = useLogin();
   const {auth, setAuth, user} = useAuthentication();
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [isHamburgerOpen, setIsHamburgerOpen] = useState(false);
 
   let actor = user_backend;
 
@@ -65,31 +69,49 @@ const Header = () => {
     }
   }, [loginStatus]);
 
+  useEffect(() => {
+    const html = document.querySelector("html")
+    if (html) html.classList.toggle("overflow-hidden", isHamburgerOpen);
+  }, [isHamburgerOpen])
+
+  useEffect(() => {
+    const closeHamburgerNavigation = () => setIsHamburgerOpen(false);
+
+    window.addEventListener("orientationchange", closeHamburgerNavigation);
+    window.addEventListener("resize", closeHamburgerNavigation);
+  }, [setIsHamburgerOpen])
+
   return (
-    <header className="fixed w-full opacity-85 z-50">
-      <nav className="bg-[#18191A] px-4 lg:px-6 py-2.5 pt-5">
-        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-          <a href="/" className="flex items-center">
-            <img
-              src="/assets/fundwise.png"
-              className="h-6 sm:h-9"
-              alt="FundWise Logo"
-            />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">
-              FundWise
-            </span>
-          </a>
-          <div className="flex items-center lg:order-2">
-            <a href="/projects">
-              <span className="self-center mx-5 text-xl font-semibold whitespace-nowrap text-white">
-                Projects
-              </span>
-            </a>
-            <a href="/">
-              <span className="self-center mx-5 text-xl font-semibold whitespace-nowrap text-white">
-                About
-              </span>
-            </a>
+    <header className="text-white fixed top-0 left-0 w-full border-b border-white-a08 z-10 backdrop-blur-[12px]">
+      <Container className='flex h-nav-height'>
+        <Link to="/" className="flex items-center md:text-lg text-md mr-3 font-bold">
+          <img src="/assets/fundwise.png" width="64" height="64" className='w-[2.2rem] h-[2.2rem] mr-3' alt="FundWise Logo"/> FundWise
+        </Link>
+
+        <nav className={classNames('h-[calc(100vh_-_var(--navigation-height))] fixed top-nav-height w-full bg-background overflow-auto transition-opacity duration-500 md:block md:relative md:top-0 left-0 md:h-nav-height md:w-auto md:bg-transparent md:overflow-hidden md:opacity-100 md:visible md:translate-x-0', isHamburgerOpen ? 'visible opacity-100 translate-x-0 bg-page-gradient' : 'invisible opacity-0 translate-x-[-100vw]')}>
+          <ul className={classNames(
+            'flex h-full flex-col [&_a]:text-md [&_a]:flex [&_a]:items-center [&_a]:h-nav-height [&_a]:w-full [&_a]:duration-500 [&_a]:translate-transform [&_li]:mx-6 [&_li]:border-b md:[&_li]:border-0 [&_li]:border-gray-dark md:hover:text-gray-500',
+            'md:flex-row md:items-center md:[&_a]:text-sm md:[&_li]:mx-3 md:[&_li]:h-full md:[&_li]:flex md:[&_li]:justify-center md:[&_li]:items-center md:[&_a]:translate-y-0',
+            isHamburgerOpen ? "[&_a]:translate-y-0" : "[&_a]:translate-y-16",
+            )}>
+            <li className='md:hover:text-white md:hover:scale-105'>
+              <Link to='/projects'>Explore</Link>
+            </li>
+            <li className='md:hover:text-white md:hover:scale-105'>
+              <Link to='/#about' onClick={() => {
+                const aboutElement = document.getElementById("about");
+                if (aboutElement) {
+                  aboutElement.scrollIntoView({ behavior: "smooth" });
+                }
+              }}>About</Link>
+            </li>
+            <li className='md:hover:text-white md:hover:scale-105'>
+              <Link to='#'>Support</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <div className='ml-auto h-full flex items-center'>
             {auth && user ? (
               <div>
                 <img
@@ -147,10 +169,10 @@ const Header = () => {
                 </div>
               </div>
             ) : (
-              <div className='hover:scale-105 duration-500'>
+              <div className='hover:scale-105 duration-500 md:text-lg text-md'>
                 <motion.button
                   onClick={login}
-                  className="px-6 py-2 rounded-md relative radial-gradient flex justify-center items-center gap-2 hover:bg-[#3A3B3C]"
+                  className=" py-2 rounded-md relative flex justify-center items-center gap-2"
                   initial={{ '--x': '100%', scale: 1 } as any}
                   animate={{ '--x': '-100%' } as any}
                   whileTap={{ scale: 0.97 }}
@@ -170,42 +192,19 @@ const Header = () => {
                     },
                   }}
                 >
-                  <img
-                    src="./assets/icp.png"
-                    className="w-full h-full object-contain absolute opacity-50"
-                    alt="Login"
-                  />
-                  <span className="text-neutral-100 tracking-wide font-medium h-full w-full block relative linear-mask text-lg">
-                    Log In
-                  </span>
-                  <span className="block absolute inset-0 rounded-md p-px linear-overlay" />
+                  <img src="./assets/icp.png" className="w-full h-full object-contain absolute opacity-50" alt="Login"/>
+                  <span className="linear-mask">Log In</span>
                 </motion.button>
               </div>
             )}
-            {/* <button data-collapse-toggle="mobile-menu-2" type="button" className="inline-flex items-center p-2 ml-1 text-sm text-gray-400 rounded-lg lg:hidden hover:bg-gray-700" aria-controls="mobile-menu-2" aria-expanded="false">
-                        <span className="sr-only">Open main menu</span>
-                        <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
-                        <svg className="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                    </button> */}
-          </div>
-          {/* <div className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1 mt-2" id="mobile-menu-2">
-                    <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0 text-lg">
-                        <li>
-                            <a href="#" className="block py-2 pr-4 pl-3 text-gray-400 border-b border-gray-100 hover:bg-gray-700 lg:hover:bg-transparent lg:border-0 lg:hover:text-white lg:p-0">Menu A</a>
-                        </li>
-                        <li>
-                            <a href="#" className="block py-2 pr-4 pl-3 text-gray-400 border-b border-gray-100 hover:bg-gray-700 lg:hover:bg-transparent lg:border-0 lg:hover:text-white lg:p-0">Menu B</a>
-                        </li>
-                        <li>
-                            <a href="#" className="block py-2 pr-4 pl-3 text-gray-400 border-b border-gray-100 hover:bg-gray-700 lg:hover:bg-transparent lg:border-0 lg:hover:text-white lg:p-0">Menu C</a>
-                        </li>
-                        <li>
-                            <a href="#" className="block py-2 pr-4 pl-3 text-gray-400 border-b border-gray-100 hover:bg-gray-700 lg:hover:bg-transparent lg:border-0 lg:hover:text-white lg:p-0">Contact</a>
-                        </li>
-                    </ul>
-                </div> */}
         </div>
-      </nav>
+
+        <button className='ml-6 md:hidden' onClick={() => setIsHamburgerOpen(!isHamburgerOpen)}>
+          <span className='sr-only'>Toggle menu</span>
+          <Hamburger isHamburgerOpen={isHamburgerOpen}/>
+        </button>
+        
+      </Container>
     </header>
   );
 };
--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 import { useState, useEffect } from 'react';
 import { useQueryCall, useUpdateCall } from '@ic-reactor/react';
 import { AuthClient } from '@dfinity/auth-client';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Button } from '@nextui-org/button';
 import { user_backend, canisterId, idlFactory } from "../../declarations/user_backend";
 import { Actor, HttpAgent } from '@dfinity/agent';
@@ -102,17 +102,17 @@ const Header = () => {
         <header className="fixed w-full opacity-75 z-50">
             <nav className="bg-[#18191A] px-4 lg:px-6 py-2.5 pt-5">
                 <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-                    <a href="/" className="flex items-center">
+                    <Link to="/" className="flex items-center">
                         <img src="/assets/fundwise.png" className="h-6 sm:h-9" alt="FundWise Logo" />
                         <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">FundWise</span>
-                    </a>
+                    </Link>
                     <div className="flex items-center lg:order-2">
-                        <a href="/projects">
+                        <Link to="/projects">
                             <span className="self-center mx-5 text-xl font-light whitespace-nowrap text-white">Projects</span>
-                        </a>
-                        <a href="/">
+                        </Link>
+                        <Link to="/">
                             <span className="self-center mx-5 text-xl font-light whitespace-nowrap text-white">About</span>
-                        </a>
+                        </Link>
                         {authenticated ? (
                             <div>
                                 <img
@@ -167,35 +167,14 @@ const Header = () => {
                             <div className="hover:scale-105 duration-500">
                                 <motion.button onClick={handleLogin} className="px-6 py-2 rounded-md relative radial-gradient flex justify-center items-center gap-2 hover:bg-[#3A3B3C]" initial={{ "--x": "100%", scale: 1 } as any} animate={{ "--x": "-100%" } as any} whileTap={{ scale: 0.97 }} transition={{ repeat: Infinity, repeatType: "loop", repeatDelay: 1, type: "spring", stiffness: 20, damping: 15, mass: 2, scale: { type: "spring", stiffness: 10, damping: 5, mass: 0.1 }, }}>
                                     <img src='./assets/icp.png' className="w-full h-full object-contain absolute opacity-50" alt="Login" />
-                                    <span className="text-neutral-100 tracking-wide font-medium h-full w-full block relative linear-mask text-lg">
+                                    <span className="text-white tracking-wide font-medium h-full w-full block relative linear-mask text-lg">
                                         Log In
                                     </span>
                                     <span className="block absolute inset-0 rounded-md p-px linear-overlay" />
                                 </motion.button>
                             </div>
                         )}
-                        {/* <button data-collapse-toggle="mobile-menu-2" type="button" className="inline-flex items-center p-2 ml-1 text-sm text-gray-400 rounded-lg lg:hidden hover:bg-gray-700" aria-controls="mobile-menu-2" aria-expanded="false">
-                        <span className="sr-only">Open main menu</span>
-                        <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
-                        <svg className="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                    </button> */}
                     </div>
-                    {/* <div className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1 mt-2" id="mobile-menu-2">
-                    <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0 text-lg">
-                        <li>
-                            <a href="#" className="block py-2 pr-4 pl-3 text-gray-400 border-b border-gray-100 hover:bg-gray-700 lg:hover:bg-transparent lg:border-0 lg:hover:text-white lg:p-0">Menu A</a>
-                        </li>
-                        <li>
-                            <a href="#" className="block py-2 pr-4 pl-3 text-gray-400 border-b border-gray-100 hover:bg-gray-700 lg:hover:bg-transparent lg:border-0 lg:hover:text-white lg:p-0">Menu B</a>
-                        </li>
-                        <li>
-                            <a href="#" className="block py-2 pr-4 pl-3 text-gray-400 border-b border-gray-100 hover:bg-gray-700 lg:hover:bg-transparent lg:border-0 lg:hover:text-white lg:p-0">Menu C</a>
-                        </li>
-                        <li>
-                            <a href="#" className="block py-2 pr-4 pl-3 text-gray-400 border-b border-gray-100 hover:bg-gray-700 lg:hover:bg-transparent lg:border-0 lg:hover:text-white lg:p-0">Contact</a>
-                        </li>
-                    </ul>
-                </div> */}
                 </div>
             </nav>
         </header>
