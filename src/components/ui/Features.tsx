92408120307e1ca84445a4447397d7c45a341641
Aliux7 <kelsonedbert@gmail.com>
2024-05-09 12:35:57 +0700

    feats: a

--- src/components/ui/Features.tsx
+++ src/components/ui/Features.tsx
@@ -139,12 +139,12 @@ const FeatureGrid = ({ features }: FeatureGridProps) => {
             <span className="block text-black md:inline">{title}</span> 
             <span>
               <div className="flex items-center">
-                <svg className="w-4 h-4 me-1" aria-hidden="true" style={{color: 'yellow'}} xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+                <svg className="w-4 h-4 me-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                    <path fill="yellow" d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
                 </svg>
-                <p className="ms-2 text-sm font-bold">4.95</p>
-                <span className="w-1 h-1 mx-2 bg-gray-500 rounded-full dark:bg-gray-400"></span>
-                <a href="#" className="text-xs">1250 Projects</a>
+                <p className="ms-2 text-sm font-bold text-gray-400">4.95</p>
+                <span className="w-1 h-1 mx-2 bg-gray-400 rounded-full"></span>
+                <a href="#" className="text-xs text-gray-400">1250 Projects</a>
             </div>
             </span>
           </div>
